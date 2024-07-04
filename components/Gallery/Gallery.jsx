996db1427c6009419c3480314feb8f5ec0109f63
GuillermoTD <guillermotd23@gmail.com>
2024-07-04 15:58:28 -0400

    Second commit

---
+++ components/Gallery/Gallery.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Gallery = () => {
+  return (
+    <>
+    <div className='gallery'>
+      <div className='gallery__cell'>cel
+        <div className="gallery__cell-img"></div>
+      </div>
+
+      <div className='gallery__cell'>cel
+        <div className="gallery__cell-img"></div>
+      </div>
+
+      <div className='gallery__cell'>cel
+        <div className="gallery__cell-img"></div>
+      </div>
+      
+    </div>
+    <style>
+      {`
+        .gallery{
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+          gap: 10px;
+          background:blue;
+          min-height:90.5vh;
+        }
+
+        .gallery__cell {
+          border: 1px solid #ccc;
+          padding: 10px;
+        }
+
+
+      `}
+    </style>
+    </>
+
+  )
+}
+
+export default Gallery
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import "../global.css"
 import {Header} from "@/components/Header/Header"
+import Gallery from "../components/Gallery/Gallery"
 export default function Home() {
   return (
   <div>
     <Header/>
-    Index
+    <Gallery/>
   </div>
   );
 }
